8f3304ed01fbb6a0ad40069f45d5c8c5d96591ca
NameInikita <name.inikita@gmail.com>
2017-07-20 23:26:21 +0300

    Создаю enum и метод для бонуса

---
+++ Manager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApplication1
+{
+    enum Manager
+    {
+        //Должности и кол-во часов отработанных за месяц
+        Manager=195,
+        Developer=220,
+        Accountant=190,
+        Secretary=185,
+        Junior=190,
+        Cleaner=180
+    }
+
+    class BonusCheck
+    {
+        //метод true/false на проверку давать бонус или нет
+        public bool Bonus(Manager man, int hours)
+        {
+            if ((int)man < hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
