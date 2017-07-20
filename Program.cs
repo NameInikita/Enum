30a422ffded1196739105e244fe5b4610d9d619e
NameInikita <name.inikita@gmail.com>
2017-07-21 00:12:42 +0300

    Все доработано по enum. До generic не добрался

--- Program.cs
+++ Program.cs
@@ -10,6 +10,53 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите кол-во часов от 0 до 220");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(@"Выберите должность по первой букве:
+m=Manager,
+d=Developer,
+a=Accountant,
+s=Secretary,
+j=Junior,
+c=Cleaner");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            int tmp=0;
+            switch (ch)
+            {
+                case'm': tmp = (int)Manager.Manager;
+                    break;
+                case 'd':tmp = (int)Manager.Developer;
+                    break;
+                case 'a':tmp = (int)Manager.Accountant;
+                    break;
+                case 's':tmp = (int)Manager.Secretary;
+                    break;
+                case 'j':tmp = (int)Manager.Junior;
+                    break;
+                case 'c':tmp = (int)Manager.Cleaner;
+                    break;
+                default: Console.WriteLine("Неверный параметр");
+                break;
+
+            }
+            Console.WriteLine("Необходимо было отработать: {0} часов , отработано по факту: {1} часов",tmp,hours);
+
+            BonusCheck m = new BonusCheck();
+
+            if (m.Bonus((Manager)tmp, hours))
+            {
+                Console.WriteLine("Дать премию");
+            }
+            else
+            {
+                Console.WriteLine("Не давать премию");
+            }
+           
+
+            Console.ReadKey();
+
         }
     }
 }
